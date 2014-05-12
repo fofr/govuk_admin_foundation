f6217e19aee6a4403fec06e4f02744ba8b3c0d4c
Paul Hayes <fofr@users.noreply.github.com>
2014-05-12 18:23:46 +0100

    Add <IE9 javascript shims for HTML5 and bootstrap
    * Precompile scripts
    * Include in conditional tag within template

--- lib/govuk_admin_foundation/engine.rb
+++ lib/govuk_admin_foundation/engine.rb
@@ -1,5 +1,12 @@
 module GovukAdminFoundation
   class Engine < ::Rails::Engine
     require 'bootstrap-sass'
+
+    initializer "govuk_admin_foundation.assets.precompile" do |app|
+      app.config.assets.precompile += %w(
+        lte-ie8.js
+      )
+    end
+
   end
 end
