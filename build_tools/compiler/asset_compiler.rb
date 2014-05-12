fe43eba11839073c43fe986205281026b560dc73
Paul Hayes <fofr@users.noreply.github.com>
2014-05-12 17:55:59 +0100

    Copy assets to app directory when compiling
    * We aren’t compiling SASS/SCSS files, mixins etc. should be accessible
    to the application using the gem

--- build_tools/compiler/asset_compiler.rb
+++ build_tools/compiler/asset_compiler.rb
@@ -13,30 +13,48 @@ module Compiler
     end
 
     def compile
-      prepare_build_dir
+      prepare_build_directory
       copy_views
+      copy_assets
     end
 
     def copy_views
       Dir.chdir @repo_root.join("source", "views") do
-        files = []
-        Dir.glob("**/*") do |file|
-          next if File.directory?(file)
-          files << file
-        end
-
-        output, status = Open3.capture2e("cp -r --parents #{files.shelljoin} #{@build_dir.join('views').to_s.shellescape}")
-
-        abort "Error copying views:\n#{output}" if status.exitstatus > 0
+        files = retrieve_files_from_directory
+        output, status = copy_files_to_subdirectory(files, "views")
+        abort "Error copying assets:\n#{output}" if status.exitstatus > 0
       end
     end
 
-    def prepare_build_dir
+    def copy_assets
+      Dir.chdir @repo_root.join("source", "assets") do
+        files = retrieve_files_from_directory
+        output, status = copy_files_to_subdirectory(files, "assets")
+        abort "Error copying assets:\n#{output}" if status.exitstatus > 0
+      end
+    end
+
+    def prepare_build_directory
       @build_dir.rmtree if @build_dir.exist?
       @build_dir.mkpath
-      @build_dir.join('assets', 'stylesheets').mkpath
-      @build_dir.join('assets', 'javascripts').mkpath
+      @build_dir.join('assets').mkpath
       @build_dir.join('views').mkpath
     end
+
+    private
+
+    def retrieve_files_from_directory
+      files = []
+      Dir.glob("**/*") do |file|
+        next if File.directory?(file)
+        files << file
+      end
+      files
+    end
+
+    def copy_files_to_subdirectory(files, subdirectory)
+      Open3.capture2e("cp -r --parents #{files.shelljoin} #{@build_dir.join(subdirectory).to_s.shellescape}")
+    end
+
   end
 end
